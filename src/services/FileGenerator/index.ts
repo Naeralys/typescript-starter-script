3174c7e4ab10d1302b95cb4fb4738f3f73825142
Adam Lind <adamlind@live.se>
2020-02-21 10:35:19 +1100

    Generates the project inside a project folder

--- src/services/FileGenerator/index.ts
+++ src/services/FileGenerator/index.ts
@@ -5,6 +5,9 @@ import IFileGenerator from "./IFileGenerator"
 
 export class FileGenerator implements IFileGenerator {
 	constructor(private projectConfig: ProjectConfig, private fileConfig: FileConfig) {}
+	public createProjectFolder = () => {
+		fs.mkdirSync(`${this.projectConfig.projectName}`)
+	}
 	public createPackage = () => {
 		const config = this.fileConfig.packageJson
 		config.name = this.projectConfig.projectName
@@ -12,31 +15,34 @@ export class FileGenerator implements IFileGenerator {
 			config.dependencies = { jest: "^25.1.0", "@types/jest": "^24.9.1" }
 		}
 		const data = JSON.stringify(config, null, 4)
-		fs.writeFileSync("package.json", data)
+		fs.writeFileSync(`${this.projectConfig.projectName}/package.json`, data)
 	}
 	public createTslint = () => {
 		const data = JSON.stringify(this.fileConfig.tslint, null, 4)
-		fs.writeFileSync("tslint.json", data)
+		fs.writeFileSync(`${this.projectConfig.projectName}/tslint.json`, data)
 	}
 	public createTsConfig = () => {
 		const data = JSON.stringify(this.fileConfig.tsconfig, null, 4)
-		fs.writeFileSync("tsconfig.json", data)
+		fs.writeFileSync(`${this.projectConfig.projectName}/tsconfig.json`, data)
 	}
 	public createIndex = () => {
 		const data = this.fileConfig.index
-		if (!fs.existsSync("src")) fs.mkdirSync("src")
-		fs.writeFileSync("src/index.ts", data)
+		if (!fs.existsSync(`${this.projectConfig.projectName}/src`))
+			fs.mkdirSync(`${this.projectConfig.projectName}/src`)
+		fs.writeFileSync(`${this.projectConfig.projectName}/src/index.ts`, data)
 	}
 	public createTestingSetup = () => {
 		const data = this.fileConfig.testing.join("\r\n")
-		if (!fs.existsSync("test")) fs.mkdirSync("test")
-		fs.writeFileSync("test/index.test.ts", data)
+		if (!fs.existsSync(`${this.projectConfig.projectName}/test`))
+			fs.mkdirSync(`${this.projectConfig.projectName}/test`)
+		fs.writeFileSync(`${this.projectConfig.projectName}/test/index.test.ts`, data)
 	}
 	public createGitignore = () => {
 		const data = this.fileConfig.gitignore.join("\r\n")
-		fs.writeFileSync(".gitignore", data)
+		fs.writeFileSync(`${this.projectConfig.projectName}/.gitignore`, data)
 	}
 	public createAllPackages = () => {
+		this.createProjectFolder()
 		this.createPackage()
 		this.createTsConfig()
 		this.createTslint()
