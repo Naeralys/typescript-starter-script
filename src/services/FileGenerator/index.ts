d2cc3947b67ffe5df8b7b218a89b0bc19d451390
Adam Lind <adamlind@live.se>
2020-02-21 16:41:26 +1100

    Added dependency injection to services

--- src/services/FileGenerator/index.ts
+++ src/services/FileGenerator/index.ts
@@ -1,10 +1,14 @@
-import * as fs from "fs"
 import { ProjectConfig } from "../../types"
 import FileConfig from "../../types/fileConfig"
+import IFileStream from "../../types/IFileStream"
 import IFileGenerator from "./IFileGenerator"
 
 export class FileGenerator implements IFileGenerator {
-	constructor(private projectConfig: ProjectConfig, private fileConfig: FileConfig) {}
+	constructor(
+		private projectConfig: ProjectConfig,
+		private fileConfig: FileConfig,
+		private fileStream: IFileStream
+	) {}
 	public createPackage = () => {
 		const config = this.fileConfig.packageJson
 		config.name = this.projectConfig.projectName
@@ -12,29 +16,29 @@ export class FileGenerator implements IFileGenerator {
 			config.dependencies = { jest: "^25.1.0", "@types/jest": "^24.9.1" }
 		}
 		const data = JSON.stringify(config, null, 4)
-		fs.writeFileSync("package.json", data)
+		this.fileStream.writeFileSync("package.json", data)
 	}
 	public createTslint = () => {
 		const data = JSON.stringify(this.fileConfig.tslint, null, 4)
-		fs.writeFileSync("tslint.json", data)
+		this.fileStream.writeFileSync("tslint.json", data)
 	}
 	public createTsConfig = () => {
 		const data = JSON.stringify(this.fileConfig.tsconfig, null, 4)
-		fs.writeFileSync("tsconfig.json", data)
+		this.fileStream.writeFileSync("tsconfig.json", data)
 	}
 	public createIndex = () => {
 		const data = this.fileConfig.index
-		if (!fs.existsSync("src")) fs.mkdirSync("src")
-		fs.writeFileSync("src/index.ts", data)
+		if (!this.fileStream.existsSync("src")) this.fileStream.mkdirSync("src")
+		this.fileStream.writeFileSync("src/index.ts", data)
 	}
 	public createTestingSetup = () => {
 		const data = this.fileConfig.testing.join("\r\n")
-		if (!fs.existsSync("test")) fs.mkdirSync("test")
-		fs.writeFileSync("test/index.test.ts", data)
+		if (!this.fileStream.existsSync("test")) this.fileStream.mkdirSync("test")
+		this.fileStream.writeFileSync("test/index.test.ts", data)
 	}
 	public createGitignore = () => {
 		const data = this.fileConfig.gitignore.join("\r\n")
-		fs.writeFileSync(".gitignore", data)
+		this.fileStream.writeFileSync(".gitignore", data)
 	}
 	public createAllPackages = () => {
 		this.createPackage()
